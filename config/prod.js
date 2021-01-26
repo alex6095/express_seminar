fc8938c91536afcdc51d4f9f52fe5823d170930a
alex6095 <l.alex6095@gmail.com>
2021-01-26 23:00:35 +0900

    DB 비밀번호 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI //헤로쿠와 동일한 변수명
+}
--- index.js
+++ index.js
@@ -3,17 +3,47 @@
 const express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
 const app = express(); //express를 실행하여 app object를 초기화 합니다.
 const port = 8000; // 사용할 포트 번호를 port 변수에 넣습니다. 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://alex6095:Sa9612266095@fresh-man.i5xks.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
-
 app.get('/', (req, res) => { // '/' 위치에 'get'요청을 받는 경우,
-  res.send('Hello World!'); // "Hello World!"를 보냅니다.
+  res.send('Hello World! Nodemon Test'); // "Hello World!"를 보냅니다.
+})
+
+app.post('/register', (req, res) => {
+
+  //회원가입 정보 클라에서 가져오면 DB에 넣는다
+  /*
+  req.body의 형태
+  {
+    id: "hello",
+    password: "123"
+  }
+  */
+ 
+  const user = new User(req.body);
+  //MongoDB 메소드 save
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 const server = app.listen(port, () => { // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
